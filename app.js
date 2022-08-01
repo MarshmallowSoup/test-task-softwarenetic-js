3beeb1fcfd996682d08db0d6d3eec9eb3fcc2f90
MarshmallowSoup <68691943+MarshmallowSoup@users.noreply.github.com>
2022-08-01 14:46:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const healthcheck = require('./healthcheck');
 
 app.get("/healthcheck", async(req,res) => {
         res.sendStatus(200);
